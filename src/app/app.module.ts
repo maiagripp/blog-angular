df2b9ad16d1550bd4656f1a9de09972101ed8d4b
Claudia Maia Gripp <44818833+maiagripp@users.noreply.github.com>
2021-11-12 18:37:43 -0300

    feature: project started

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BodyComponent } from './shared/components/body/body.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
